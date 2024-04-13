44fc9f38c88b3a8371597b4cdee9a2968b7ea234
FuzimakiSyunta <115062898+FuzimakiSyunta@users.noreply.github.com>
2024-04-14 03:17:43 +0900

    camera

--- DirectXGame/FollowCamera.cpp
+++ DirectXGame/FollowCamera.cpp
@@ -12,29 +12,24 @@ void FollowCamera::Initialize() {
 }
 
 void FollowCamera::Update() {
-	//// ゲームパッドの状態を得る変数
-	//XINPUT_STATE joyState;
-	//角度
+	////角度
 	viewProjection_.rotation_.x = 90.0f;
-	viewProjection_.rotation_.y = -15.0f;
+	
 	if (target_) {
 		// 追従対象からカメラまでのオフセット
-		Vector3 offset = {0.0f, 25.0f, 10.0f};
-		offset = TransformNormal(offset, MakeRotateYmatrix(viewProjection_.rotation_.y)); // 途中
+		Vector3 offset = {0.0f, 55.0f, 20.0f};
+		offset = TransformNormal(offset, MakeRotateYmatrix(viewProjection_.rotation_.y));
 		// 座標をコピーしてオフセット分ずらす
 		viewProjection_.translation_ = Add(target_->translation_, offset);
 	}
-	//if (Input::GetInstance()->GetJoystickState(0, joyState)) {
-	//	// 速さ
-	//	const float speed = 0.6f;
-	//	// 移動量
-	//	Vector3 move = {
-	//	    (float)joyState.Gamepad.sThumbRX / SHRT_MAX * -speed, 0.0f,
-	//	    (float)joyState.Gamepad.sThumbRY / SHRT_MAX * -speed};
-	//	// 移動量に速さを反映
-	//	move = Multiply(speed, Normalize(move));
-	//	move = TransformNormal(move, MakeRotateYmatrix(viewProjection_.rotation_.x));
-	//}
+	// ゲームパッドの状態を得る変数
+	XINPUT_STATE joyState;
+
+	if (Input::GetInstance()->GetJoystickState(0, joyState)) {
+		// 速さ
+		const float rotate = 0.04f;
+		viewProjection_.rotation_.y += joyState.Gamepad.sThumbRX / SHRT_MAX * rotate;
+	}
 	
 	// 行列の更新
 	viewProjection_.UpdateMatrix();
