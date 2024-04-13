596425500870c1af3e550b8166bc4657a10fd687
FuzimakiSyunta <115062898+FuzimakiSyunta@users.noreply.github.com>
2024-04-14 02:26:56 +0900

    l

--- DirectXGame/FollowCamera.cpp
+++ DirectXGame/FollowCamera.cpp
@@ -12,6 +12,9 @@ void FollowCamera::Initialize() {
 }
 
 void FollowCamera::Update() {
+	//// ゲームパッドの状態を得る変数
+	//XINPUT_STATE joyState;
+	//角度
 	viewProjection_.rotation_.x = 90.0f;
 	viewProjection_.rotation_.y = -15.0f;
 	if (target_) {
@@ -21,6 +24,17 @@ void FollowCamera::Update() {
 		// 座標をコピーしてオフセット分ずらす
 		viewProjection_.translation_ = Add(target_->translation_, offset);
 	}
+	//if (Input::GetInstance()->GetJoystickState(0, joyState)) {
+	//	// 速さ
+	//	const float speed = 0.6f;
+	//	// 移動量
+	//	Vector3 move = {
+	//	    (float)joyState.Gamepad.sThumbRX / SHRT_MAX * -speed, 0.0f,
+	//	    (float)joyState.Gamepad.sThumbRY / SHRT_MAX * -speed};
+	//	// 移動量に速さを反映
+	//	move = Multiply(speed, Normalize(move));
+	//	move = TransformNormal(move, MakeRotateYmatrix(viewProjection_.rotation_.x));
+	//}
 	
 	// 行列の更新
 	viewProjection_.UpdateMatrix();
