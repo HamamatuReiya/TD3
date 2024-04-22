5bd0e74ababddea9839573ca3d48835bcb3d4c6e
HamamatuReiya <115063040+HamamatuReiya@users.noreply.github.com>
2024-04-22 11:08:27 +0900

    地面と丸太を調整

--- DirectXGame/ForestWood.cpp
+++ DirectXGame/ForestWood.cpp
@@ -5,7 +5,7 @@ void ForestWood::Initialize(Model* model) {
 	worldTransform_.Initialize();
 	worldTransform_.scale_ = {1.0f, 1.0f, 1.0f};
 	worldTransform_.rotation_ = {0.0f, 0.0f, 0.0f};
-	worldTransform_.translation_ = {0.0f, 0.0f, 0.0f};
+	worldTransform_.translation_ = {0.0f, 0.0f, 20.0f};
 }
 
 void ForestWood::Update() {
