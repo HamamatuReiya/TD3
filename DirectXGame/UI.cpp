0c36a8ae3e2ce860f5d1b9e8e0c6aaf2a1867257
SaitoYuuki48 <115055003+SaitoYuuki48@users.noreply.github.com>
2024-05-15 17:00:16 +0900

    強化画面の画像追加

--- DirectXGame/UI.cpp
+++ DirectXGame/UI.cpp
@@ -6,6 +6,16 @@ void UI::Initialize() {
 	uint32_t textureWindow = TextureManager::Load("Window.png");
 	uint32_t textureExclamationMark = TextureManager::Load("!.png");
 	uint32_t textureButtonHint = TextureManager::Load("Buttonhint.png");
+	//素材　画像
+	//石
+	uint32_t textureStone = TextureManager::Load("material/stone.png");
+	//金
+	uint32_t textureGold = TextureManager::Load("material/gold.png");
+	//樹脂
+	uint32_t textureJushi = TextureManager::Load("material/jushi.png");
+	//貝
+	uint32_t textureShell = TextureManager::Load("material/shell.png");
+
 	
 	// スプライト生成
 	spriteWindow_ =
@@ -16,10 +26,25 @@ void UI::Initialize() {
 	// スプライト生成
 	spriteButtonHint_ = Sprite::Create(textureButtonHint, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.0f, 0.0f});
 	
+	//
+	spriteStone_ =
+	    Sprite::Create(textureStone, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.0f, 0.0f});
+
+	spriteGold_ = Sprite::Create(textureGold, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.0f, 0.0f});
+
+	spriteJushi_ = Sprite::Create(textureJushi, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.0f, 0.0f});
+
+	spriteShell_ = Sprite::Create(textureShell, {0.0f, 0.0f}, {1.0f, 1.0f, 1.0f, 1.0f}, {0.0f, 0.0f});
 }
 
 void UI::Draw() { 
-	spriteWindow_->Draw(); }
+	spriteWindow_->Draw();
+
+	spriteStone_->Draw();
+	spriteGold_->Draw();
+	spriteJushi_->Draw();
+	spriteShell_->Draw();
+}
 
 void UI::ExclamationMarkDraw() { spriteExclamationMark_->Draw(); }
 
